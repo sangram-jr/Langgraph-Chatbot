b22ca9d4a5292c6ac71dd70d45733900dedca250
sangram-jr <sangrambera58@gmail.com>
2026-09-20 16:38:24 +0530

    create a basic chatbot with UI

---
+++ streamlit_frontend.py
@@ -0,0 +1,41 @@
+# st.session_state => streamlit state
+# st.chat_input => input box 
+# st.chat_message  => Used to display a chat message in a conversational UI.
+# st.text => Used to display plain text in your Streamlit app.
+
+
+import streamlit as st
+# import chatbot from langgraph_backend file
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+# st.session_state -> dict -> 
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+#{'role': 'user', 'content': 'Hi'}
+#{'role': 'assistant', 'content': 'Hi=ello'}
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    
+    ai_message = response['messages'][-1].content
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    with st.chat_message('assistant'):
+        st.text(ai_message)
