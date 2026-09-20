2d2ea1769de99dda5cc2eec33c1b6aa65d1a8c53
sangram-jr <sangrambera58@gmail.com>
2026-09-20 17:43:29 +0530

    add sqlite db in chatbot & also tracking it with langsmith

---
+++ langgraph_backend_sqlite.py
@@ -0,0 +1,49 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import os
+import sqlite3
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="openai/gpt-oss-120b",
+    api_key=os.getenv("GROQ_API_KEY"),
+    temperature=0
+)
+
+# state
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+
+# initiate connection with sqlite db
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+# Checkpointer
+checkpointer = SqliteSaver(conn=conn)
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    # Creates an empty set ->all_threads, to store thread IDs.
+    all_threads = set()
+    for checkpoint in checkpointer.list(None): # None = No configuration filter, allowing you to iterate through all checkpoints across all threads.
+        # only fetch all the thread_id 
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
+
